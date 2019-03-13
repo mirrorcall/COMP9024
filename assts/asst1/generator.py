44dfb29d8155421ec823bf08144978c108caa91b
chrisly07 <chrizly07@gmail.com>
2019-03-13 16:03:20 +1100

    custom assignment 1 autotest script

--- assts/asst1/generator.py
+++ assts/asst1/generator.py
@@ -3,6 +3,8 @@ from random import seed,sample,randint
 seed(10)
 for x in range(1, 4):
 	f=open("test"+str(x)+".txt", "w")
+	# change the range and randint to take effct on
+	# different range of lists.
 	nums=sample(range(-99999,99999),randint(10,50))
 	[f.write(str(x)+"\n") for x in nums]
 	f.close()
